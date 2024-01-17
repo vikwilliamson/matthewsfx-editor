68f834f3ba8762923e9b6f74407e7af0e05c4e12
Vik Williamson <williamson.vik@gmail.com>
2024-01-16 18:36:20 -0800

    Add sidebar component code; Add Bank and Preset types, plus mock data;

--- src/components/chrome/LeftSideBar.tsx
+++ src/components/chrome/LeftSideBar.tsx
@@ -1,5 +1,26 @@
-export default function LeftSideBar() {
+import { Bank } from "../containers/App";
+
+type LeftSideBarProps = {
+    banks: Bank[];
+}
+
+const LeftSideBar: React.FC<LeftSideBarProps> = ({ banks }: LeftSideBarProps) => {
+
     return(
-        <></>
+        <div style={{ border: '2px solid black', height: '100vh', width: '100%' }}>
+            <h3 style={{ textDecoration: 'underline' }}>PRESETS</h3>
+            {banks.map((bank, i) => (
+                <div key={i} style={{ border: '1px solid black', marginBottom: '1rem' }}>
+                    <p>{bank.bankName}</p>
+                    {bank.presets.map((preset, x) => (
+                        <div key={x} style={{ border: '0.5px solid black' }}>
+                            <p>{`${x+1}: ${preset.presetName}`}</p>
+                        </div>
+        ))}
+                </div>
+        ))}
+        </div>
     )
 }
+
+export default LeftSideBar;
--- src/components/containers/App.tsx
+++ src/components/containers/App.tsx
@@ -10,9 +10,22 @@ import Grid from '@mui/material/Grid';
 import '../../styles/App.css';
 // ASSETS/DATA
 
+export type Bank = {
+  bankName: string;
+  presets: Preset[];
+}
+
+export type Preset = {
+  presetName: string;
+  presetDescription?: string;
+  messages: Array<Array<string>>;
+}
+
 const App: React.FC = () => {
   const [midiAccessObject, setMidiAccessObject] = useState<WebMidi.MIDIAccess | null>(null);
   const [deviceStatus, setDeviceStatus] = useState<string>('disconnected');
+  const [presets, setPresets] = useState<Bank[]>([{ bankName: 'Bank 1', presets: [{ presetName: 'TestPreset1', presetDescription: 'A sample test preset', messages: [['192', '0'], ['192', '1']] }, { presetName: 'TestPreset2', presetDescription: 'Sample text for a desc', messages: [['192', '1'], ['192', '0']] }]},
+   { bankName: 'Bank 2', presets: [{ presetName: 'Test2Preset1', presetDescription: 'A description for the preset', messages: [['192', '1', '192', '0']] }] }]);
 
   useEffect(() => {
     const initMidi = async () => {
@@ -37,7 +50,7 @@ const App: React.FC = () => {
     return (
     <Grid className="App" container columns={16}>
     <Grid item xs={2}>
-      <LeftSideBar />
+      <LeftSideBar banks={presets} />
     </Grid>
     <Grid item xs={14}>
       <AppHeader status={deviceStatus} />
